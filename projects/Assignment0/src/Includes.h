43a8604cef3978705cbd169d1ae86778cc035a35
ScientX <tori789@gmail.com>
2014-03-15 11:16:53 +0000

    comments

--- projects/Assignment0/src/Includes.h
+++ projects/Assignment0/src/Includes.h
@@ -1,5 +1,8 @@
 #pragma once
 
+
+//This define is used to allow usage of fopen and fwrite functions
+//in the savebmp function, when SOIL is added this can go away.
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <iostream>
